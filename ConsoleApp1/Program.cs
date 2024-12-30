f59db5e004b6556dab630d0b42bbce20315a6781
julezjazz <55053987+julezjazz@users.noreply.github.com>
2024-12-30 14:40:05 -0500

    Displaying divisors and stating if a number is prime

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,29 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-// Changing phrases to camelCase
-// practice with substrings and ToUpper
-using System.Runtime.InteropServices;
+﻿using System.Runtime.InteropServices;
+using System.Linq;
+using System.Linq.Expressions;
 
-string str = "the-stealth-warrior";
+// int n = Int32.Parse(Console.ReadLine());
 
-char[] delimiterChars = {'_', '-'};
+int n = 17;
 
-string[] words = str.Split(delimiterChars);
+List<int> list = new List<int>();
+int max = n / 2;
 
-str = words[0];
 
-for (int i = 1; i < words.Length; i++) {
-   string firstLetter = words[i].Substring(0, 1).ToUpper();
-   string fullWord = firstLetter + words[i].Substring(1);
-   str += fullWord;
+for (int i = 2; i <= max; i++) {
+    if (n % i == 0) 
+    {
+        list.Add(i);
+        Console.WriteLine(list[^1]);
+    }  
 }
 
+if (list.Count == 0) {
+    Console.WriteLine(n + " is a prime number");
+}
+
+// Console.WriteLine(list[^1]);
 
 
 
 
 
+// See https://aka.ms/new-console-template for more information
+// Changing phrases to camelCase
+// practice with substrings and ToUpper
+// using System.Runtime.InteropServices;
 
+// string str = "the-stealth-warrior";
 
+// char[] delimiterChars = {'_', '-'};
+
+// string[] words = str.Split(delimiterChars);
+
+// str = words[0];
+
+// for (int i = 1; i < words.Length; i++) {
+//    string firstLetter = words[i].Substring(0, 1).ToUpper();
+//    string fullWord = firstLetter + words[i].Substring(1);
+//    str += fullWord;
+// }
 
 
 
